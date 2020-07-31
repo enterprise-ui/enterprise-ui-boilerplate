a0501c41615acd33a4189a4aa1caa68d494ed81a
FeHHix <ifenix0@gmail.com>
2020-07-31 20:22:26 +0300

    Fixed build

--- packages/app/src/config.js
+++ packages/app/src/config.js
@@ -3,13 +3,13 @@ module.exports = {
   '/films': {
     injectedReducerKey: 'films',
     injectedSagaKey: 'films',
-    loadModule: () => import(/* webpackIgnore: true */ '/films/bundle.js'),
-    moduleName: 'films',
+    loadModule: () => import('@enterprise-ui/films'),
+    useSrc: true,
   },
   '/news': {
     injectedReducerKey: 'news',
     injectedSagaKey: 'news',
     loadModule: () => import(/* webpackIgnore: true */ '/news/bundle.js'),
     moduleName: 'news',
-  }
+  },
 };
