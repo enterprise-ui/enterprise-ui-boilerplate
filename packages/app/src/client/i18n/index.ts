aefa965609c502d55c09335916d773f8ed4ae343
FeHHix <ifenix0@gmail.com>
2020-08-14 22:33:02 +0300

    Added i18n support.

--- packages/app/src/client/i18n/index.ts
+++ packages/app/src/client/i18n/index.ts
@@ -1,20 +1,52 @@
+import 'reflect-metadata';
+import { II18n } from '@enterprise-ui/appcore';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
 
-i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .init({
-    whitelist: ['en', 'de'],
-    fallbackLng: 'en',
-    resources: {
-      en: { translation: require('./en/translation.json') },
-      de: { translation: require('./de/translation.json') },
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+import { injectable } from 'inversify';
 
-export default i18n;
+export interface II18nConfig {
+  defaultNS: 'NEWS';
+  ns: ['NEWS'];
+}
+
+@injectable()
+export class I18NService implements II18n<II18nConfig> {
+  private _i18n: i18n.i18n = i18n;
+
+  constructor() {
+    this._i18n
+      .use(LanguageDetector)
+      .use(Backend)
+      .init({
+        backend: {
+          loadPath: 'locales/{{lng}}/{{ns}}.json',
+        },
+        load: 'languageOnly',
+        fallbackLng: 'ru',
+        contextSeparator: '#',
+        interpolation: {
+          escapeValue: false,
+        },
+        react: {
+          useSuspense: false,
+        },
+        debug: true,
+      });
+  }
+
+  init(config: II18nConfig) {
+    const { defaultNS } = config;
+
+    this._i18n.setDefaultNamespace(defaultNS);
+  }
+
+  loadNamespaces(ns: string[]) {
+    return this._i18n.loadNamespaces(ns);
+  }
+
+  get i18n(): i18n.i18n {
+    return this._i18n;
+  }
+}
---
+++ packages/news/src/i18n.config.ts
@@ -0,0 +1,4 @@
+export const i18nConfig = {
+  ns: ['NEWS'],
+  defaultNS: 'NEWS',
+};
